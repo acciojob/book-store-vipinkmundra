ebd3ddbf477e98cba2f8e44c5f96b74b61d4a4fa
vipin <vipinkmundra@gmail.com>
2023-01-31 23:36:37 +0530

    solution4

--- src/main/java/com/driver/Book.java
+++ src/main/java/com/driver/Book.java
@@ -14,8 +14,8 @@ public class Book {
 
      }
 
-     public Book(int id, String name, String genre, String author) {
-          this.id = id;
+     public Book(String name, String genre, String author) {
+          this.id += id;
           this.name = name;
           this.genre = genre;
           this.author = author;
